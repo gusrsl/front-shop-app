a60bf80154d9589d34602a0af8feb2acebdb0c0d
Gustavo Rodriguez <89959859+gusrsl@users.noreply.github.com>
2024-04-05 13:16:41 -0500

    Diseno producto
    Slider Home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,18 @@
 /* eslint-disable no-trailing-spaces */
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Swiper } from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterViewInit {
 
   isLoggedIn: boolean | undefined;
-
+  images = ['assets/slider1.png'];
   constructor(private authService: AuthService) { }
-
-
 
   ngOnInit() {
     this.authService.isLoggedIn.subscribe(loggedIn => {
@@ -21,5 +20,30 @@ export class HomePage implements OnInit {
     });
   }
 
+  ngAfterViewInit() {
+    const swiper = new Swiper('.swiper-container', {
+      // Default parameters
+      slidesPerView: 1,
+      spaceBetween: 10,
 
+      // Responsive breakpoints
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        }
+      }
+    });
+  }
 }
