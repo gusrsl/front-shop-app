1b8ef960beb547b6fc9207f3f150e905b24ad1a3
Gustavo Rodriguez <89959859+gusrsl@users.noreply.github.com>
2024-05-02 01:06:27 -0500

    UPDATE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3000/' // reemplaza esto con tu URL de API real
+  API_URL: 'https://gustavo-rodriguez.tech/' // reemplaza esto con tu URL de API real
 
 };
